03abbbbe8621ff773f4642fec1953c5237e03eae
Darrell Marshall <darrellmarshall@gmail.com>
2025-05-01 11:15:05 -0400

    Moved unit test functions into different file test_example.py

--- example.py
+++ example.py
@@ -1,8 +1,6 @@
+#!/usr/bin/env python3
 
 def git_operation():
  print("I am adding example.py file to the remote repository.")
-
-def test_git_operation():
-    assert git_operation() == None
 
 git_operation()
---
+++ test_example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import example
+
+def test_git_operation():
+    assert example.git_operation() == None
+
